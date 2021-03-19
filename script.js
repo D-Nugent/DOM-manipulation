e665d77b7a9a744475ac8970d1eeb96553e4e9af
David Nugent <65925093+brainstation-oll@users.noreply.github.com>
2021-03-19 14:08:19 -0700

    Updated content to reflect nested elements and addiing additional values

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
-const selectedNavBar = document.querySelector('.navigation__bar');
+const selectedNavBar = document.querySelector('.navigation__bar')
+const profileNav = document.createElement('li');
+profileNav.classList.add('navigation__links');
+profileNav.innerHTML = 'Red Monkey';
 
-const profileLink = document.createElement('li');
-profileLink.classList.add('navigation__links');
-profileLink.innerText = 'Profile'
-selectedNavBar.appendChild(profileLink);
+selectedNavBar.appendChild(profileNav);
+selectedNavBar.style.width = '60vw';
 
-selectedNavBar.style.width = '20vw';
+const thingsToDo = [
+    {
+      name: 'Cut the yard',
+      priority: 'Not so bad',
+    },
+    {
+      name: 'Teach Mandohort',
+      priority: 'urgent!',
+    },
+    {
+      name: 'Hang Shelves',
+      priority: 'Some point this week',
+    },
+    {
+      name: 'Drink Tea (or Beer)',
+      priority: 'urgent!',
+    }
+];
+const extraThingToDo = 
+    {
+      name: 'Write more things for the to do list!',
+      priority: 'As soon as there are more things to do!',
+    };
 
-const thingsToDo = [`Review material`, `Study`, `Exercise`, `Relax`]
 
-function showThingsToDo(){
-    const todoListContainer = document.querySelector('.todo__list');
-    thingsToDo.forEach(todo => {
-        const toDoItem = document.createElement('li');
+function showThingsToDo(todoArray) {
+    const selectedToDo = document.querySelector('.todo__list');
+    selectedToDo.innerHTML = '';
+    todoArray.forEach(task => {
+        const toDoItem = document.createElement(`li`);
         toDoItem.classList.add('todo__list-item');
-        toDoItem.innerText = todo;
-        todoListContainer.appendChild(toDoItem);
-    });
+        const toDoItemName = document.createElement('h4');
+        toDoItemName.classList.add('todo__list-item');
+        toDoItemName.innerText = task.name;
+        const toDoItemPriority = document.createElement('p');
+        toDoItemPriority.classList.add('todo__list-item');
+        toDoItemPriority.innerText = task.priority;
+        selectedToDo.appendChild(toDoItem);
+        toDoItem.appendChild(toDoItemName);
+        toDoItem.appendChild(toDoItemPriority);
+    })
 }
 
+showThingsToDo(thingsToDo);
+    
 const todoLaunch = document.querySelector('.todo__launch');
-todoLaunch.addEventListener('click', function () {
-    showThingsToDo()
+todoLaunch.addEventListener('click', function() {
+    thingsToDo.push(extraThingToDo);
+    showThingsToDo(thingsToDo);
 })
